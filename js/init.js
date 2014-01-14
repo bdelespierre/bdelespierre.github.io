da64c14af039df81931b1ff0d73317c216ce3f1a
Benjamin Delespierre <benjamin.delespierre@gmail.com>
2014-01-14 18:28:26 +0100

    minor refactoring; curriculum completion

--- js/init.js
+++ js/init.js
@@ -18,7 +18,7 @@
 				useOrientation: true,
 				breakpoints: {
 					'widest':	{ range: '1881-', hasStyleSheet: false, containers: 1400, grid: { gutters: 40 } },
-					'wide':	{ range: '961-1880', containers: 1200, grid: { gutters: 40 } },
+					'wide':	    { range: '961-1880', containers: 1200, grid: { gutters: 40 } },
 					'normal':	{ range: '961-1620', containers: 960, grid: { gutters: 40 } },
 					'narrow':	{ range: '961-1320', containers: 'fluid', grid: { gutters: 20 } },
 					'narrower':	{ range: '-960', containers: 'fluid', grid: { gutters: 15 } },
@@ -188,6 +188,62 @@
 			});
 
 		};
+
+	// inview event
+	    function getViewportHeight() {
+	        var height = window.innerHeight; // Safari, Opera
+	        var mode = document.compatMode;
+
+	        if ( (mode || !$.support.boxModel) ) { // IE, Gecko
+	            height = (mode == 'CSS1Compat') ?
+	            document.documentElement.clientHeight : // Standards
+	            document.body.clientHeight; // Quirks
+	        }
+
+	        return height;
+	    }
+
+	    $(window).scroll(function () {
+	        var vpH = getViewportHeight(),
+	            scrolltop = (document.documentElement.scrollTop ?
+	                document.documentElement.scrollTop :
+	                document.body.scrollTop),
+	            elems = [];
+	        
+	        // naughty, but this is how it knows which elements to check for
+	        $.each($.cache, function () {
+	            if (this.events && this.events.inview) {
+	                elems.push(this.handle.elem);
+	            }
+	        });
+
+	        if (elems.length) {
+	            $(elems).each(function () {
+	                var $el = $(this),
+	                    top = $el.offset().top,
+	                    height = $el.height(),
+	                    inview = $el.data('inview') || false;
+
+	                if (scrolltop > (top + height) || scrolltop + vpH < top) {
+	                    if (inview) {
+	                        $el.data('inview', false);
+	                        $el.trigger('inview', [ false ]);                        
+	                    }
+	                } else if (scrolltop < (top + height)) {
+	                    if (!inview) {
+	                        $el.data('inview', true);
+	                        $el.trigger('inview', [ true ]);
+	                    }
+	                }
+	            });
+	        }
+	    });
+	    
+	    // kick the event to pick up any elements already in view.
+	    // note however, this only works if the plugin is included after the elements are bound to 'inview'
+	    $(function () {
+	        $(window).scroll();
+	    });
 
 // Ready stuff
 	jQuery(function() {
